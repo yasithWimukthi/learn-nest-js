83dc4570f1d231d69fb204149cdf9865c4e2e003
yasith <it19966922@my.sliit.lk>
2022-03-20 16:17:02 +0530

    create user entity

---
+++ src/user/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  @Column()
+  id: number;
+
+  @Column()
+  fist_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column({
+    unique: true,
+  })
+  email: string;
+
+  @Column()
+  password: string;
+}
