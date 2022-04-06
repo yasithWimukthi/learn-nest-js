f5f956c26cdc926951d15cb92ec6d1d927bd3644
yasith <it19966922@my.sliit.lk>
2022-04-06 23:36:29 +0530

    save user details

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,21 @@
 import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import bcrypt from 'bcryptjs';
 
 @Controller('api')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Post('register')
-  register(@Body() user: any) {
+  async register(@Body() user: any) {
     if (user.password !== user.confirm_password) {
       throw new BadRequestException('Passwords do not match');
     }
-    return user;
+    return this.userService.save({
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      password: await bcrypt.hash(user.password, 12),
+    });
   }
 }
